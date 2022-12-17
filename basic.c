4dc29d5eca95651d4e0e4c864161c4036c0c0f93
Programmer-govind <117900869+Programmer-govind@users.noreply.github.com>
2022-12-17 13:01:47 +0530

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,12 @@
+//Basic
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    clrscr();
+    printf("**********************************\n");
+    printf("\nHello, Welcome to the world of Programming.\n");
+    printf("\n**********************************");
+    getch();
+}

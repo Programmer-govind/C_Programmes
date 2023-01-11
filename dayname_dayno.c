e6b13db753e20384d5a10e69e04080d61b705fe0
Programmer-govind <117900869+Programmer-govind@users.noreply.github.com>
2023-01-11 11:15:54 +0530

    Add files via upload

---
+++ dayname_dayno.c
@@ -0,0 +1,29 @@
+//Using switch function to get dayname according to day number
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int num;
+    printf("Enter a day number: ");
+    scanf("%d", &num);
+    switch (num)
+    {
+    case 1: printf("Monday");
+    break;
+    case 2: printf("Tuesday");
+    break;
+    case 3: printf("Wednesday");
+    break;
+    case 4: printf("Thursday");
+    break;
+    case 5: printf("Friday");
+    break;
+    case 6: printf("Saturday");
+    break;
+    case 7: printf("Sunday");
+    break;
+        default: printf("Not a valid day number");
+        break;
+    }
+}
